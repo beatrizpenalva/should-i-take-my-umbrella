5ea88b8ea44e99ac9ba116e27bf8feb736391cda
Beatriz Penalva <biapenalva@gmail.com>
2021-07-24 19:51:36 -0300

    creeat component weather info

--- src/App.js
+++ src/App.js
@@ -1,8 +1,84 @@
 import React from 'react';
+import WeatherInfo from './components/WeatherInfo';
 
 function App() {
   return (
-    <h1>Hello, world</h1>
+    <>
+      <form className="location-info">
+        <label>
+          What is your location?
+          <input type="text" placeholder="Example: Salvador, BR" />
+        </label>
+      </form>
+      
+      <i class="fas fa-circle"></i>
+      <p>Clear sky</p>
+
+      <section className="weather-info">
+        <h1>22º C</h1>
+
+        <section className="tempeture-info">
+          <section>
+            <p>min</p>
+            <h3>18º C</h3>
+          </section>
+
+          <section>
+            <p>max</p>
+            <h3>24º C</h3>
+          </section>
+        </section>
+
+        <button>More info</button>
+
+        <section className="details-info">
+        <li className="info">
+          <p><i class="fas fa-wind"></i></p>
+          <p>3m/s</p>
+          <p>Wind</p>
+        </li>
+
+        <li className="info">
+          <p><i class="fas fa-wind"></i></p>
+          <p>3m/s</p>
+          <p>Humidity</p>
+        </li>
+
+        <li className="info">
+          <p><i class="fas fa-wind"></i></p>
+          <p>3m/s</p>
+          <p>Pressure</p>
+        </li>
+
+        <li className="info">
+          <p><i class="far fa-sun"></i></p>
+          <p>3m/s</p>
+          <p>Sunrise</p>
+        </li>
+
+        <li className="info">
+          <p><i class="far fa-sun"></i></p>
+          <p>3m/s</p>
+          <p>Sunset</p>
+        </li>
+      </section>
+      </section>
+    
+      <section className="week-info">
+        <WeatherInfo />
+        <WeatherInfo />
+        <WeatherInfo />
+        <WeatherInfo />
+        <WeatherInfo />
+        <WeatherInfo />
+        <WeatherInfo />
+        <WeatherInfo />
+        <WeatherInfo />
+        <WeatherInfo />
+        <WeatherInfo />
+        <WeatherInfo />
+      </section>
+    </>
   );
 }
 
---
+++ src/components/WeatherInfo.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const WeatherInfo = () => {
+    return (
+        <>
+            <h4>Fri</h4>
+            <section>
+                <i class="fas fa-wind"></i>
+                <p>17º C</p>
+                <p>14º C</p>
+            </section>
+        </>
+    )
+}
+
+export default WeatherInfo;
