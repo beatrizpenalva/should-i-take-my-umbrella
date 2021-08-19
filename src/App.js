2e31e3dc051bf9b7d3ea4c6479f299fb509432c8
Beatriz Penalva <biapenalva@gmail.com>
2021-08-19 20:34:06 -0300

    feature: fetch API

--- src/App.js
+++ src/App.js
@@ -1,20 +1,40 @@
-import React from 'react';
-import WeatherInfo from './components/WeatherInfo/index';
-import WeatherDetails from './components/WeatherDetails/index';
+import React, { useState } from "react";
+import WeatherInfo from "./components/WeatherInfo/index";
+import WeatherDetails from "./components/WeatherDetails/index";
 
 function App() {
+  const [city, setCity] = useState("");
+
+  const callAPI = (e) => {
+    e.preventDefault();
+    fetch(
+      `https://api.openweathermap.org/data/2.5/weather?q=${city.toLowerCase()}&APPID=7c8b054ddd8f88293b1e0e10e75ba18d`
+    )
+      .then((response) => response.json())
+      .then((res) => console.log(res));
+    // .then(json => console.log(json))
+  };
+
+  const setCityInput = (event) => {
+    setCity(event.target.value);
+  };
+
   return (
     <>
-      <form className="location-info">
+      <form className="location-info" onSubmit={callAPI}>
         <label>
           What is your location?
-          <input type="text" placeholder="Example: Salvador, BR" />
+          <input
+            type="text"
+            placeholder="Example: Salvador, BR"
+            onChange={setCityInput}
+          />
         </label>
       </form>
-      
+
       <section className="container">
         <section className="resume">
-          <i class="fas fa-circle"></i>
+          <i className="fas fa-circle"></i>
           <h3>Clear sky</h3>
         </section>
 
@@ -32,29 +52,31 @@ function App() {
             </section>
           </section>
 
-          <button className="toggle-button">More info<i class="fas fa-chevron-down"></i></button>
+          <button className="toggle-button">
+            More info<i className="fas fa-chevron-down"></i>
+          </button>
           <section className="details-section">
-              <WeatherDetails />
-              <WeatherDetails />
-              <WeatherDetails />
-              <WeatherDetails />
-            </section>
+            <WeatherDetails />
+            <WeatherDetails />
+            <WeatherDetails />
+            <WeatherDetails />
+          </section>
         </section>
-      
+
         <section className="week-section">
-        <WeatherInfo />
-        <WeatherInfo />
-        <WeatherInfo />
-        <WeatherInfo />
-        <WeatherInfo />
-        <WeatherInfo />
-        <WeatherInfo />
-        <WeatherInfo />
-        <WeatherInfo />
-        <WeatherInfo />
-        <WeatherInfo />
-        <WeatherInfo />
-      </section>
+          <WeatherInfo />
+          <WeatherInfo />
+          <WeatherInfo />
+          <WeatherInfo />
+          <WeatherInfo />
+          <WeatherInfo />
+          <WeatherInfo />
+          <WeatherInfo />
+          <WeatherInfo />
+          <WeatherInfo />
+          <WeatherInfo />
+          <WeatherInfo />
+        </section>
       </section>
     </>
   );
