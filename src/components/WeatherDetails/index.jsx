c77173a4a7b54d768f082c570e1d6627889faa03
Beatriz Penalva <biapenalva@gmail.com>
2021-08-26 23:25:45 -0300

    fix: change icons of weather details

--- src/components/WeatherDetails/index.jsx
+++ src/components/WeatherDetails/index.jsx
@@ -2,20 +2,21 @@ import React from "react";
 import "./style.css";
 
 const WeatherDetails = ({contents, info}) => {
+    const typeOfInfo = contents;
+    function getIcon(contents) {
+        if (typeOfInfo.toLowerCase() === "wind") return (<i className="fas fa-wind icon"></i>)
+        if (typeOfInfo.toLowerCase() === "humidity") return (<i className="fas fa-tint icon"></i>)
+        if (typeOfInfo.toLowerCase() === "sunrise") return (<i className="far fa-sun icon"></i>)
+        if (typeOfInfo.toLowerCase() === "sunset") return (<i className="fas fa-sun icon"></i>)
+    }
+
     return (
         <section className="details-info">
-            <i className="fas fa-wind icon"></i>
+            {getIcon()}
             <p>{info}</p>
             <p className="info">{contents}</p>
         </section>
     )
 }
-
-/*
-    <i class="fas fa-tint"></i>
-    <i className="fas fa-wind icon"></i>
-    <i class="fas fa-sun"></i>
-    <i class="far fa-sun"></i>
-*/
 
 export default WeatherDetails;
