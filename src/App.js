07bed9b436a9d7231be302ca05c6e76151ae58e6
Beatriz Penalva <biapenalva@gmail.com>
2021-08-28 21:04:35 -0300

    chore: dealing with multiple requests by order

--- src/App.js
+++ src/App.js
@@ -1,6 +1,11 @@
-import React, { useEffect, useState } from "react";
-import { getCurrentWeather, getCordinates, getHistoricalWeather, getForecastWeather} from "./services"
-import { Logo, WeatherDetails, WeatherIcon, WeatherInfo} from "./components/";
+import React, { useState } from "react";
+import {
+  getCurrentWeather,
+  getCordinates,
+  getHistoricalWeather,
+  getForecastWeather,
+} from "./services";
+import { Logo, WeatherDetails, WeatherIcon, WeatherInfo } from "./components/";
 
 function App() {
   const [city, setCity] = useState("");
@@ -16,85 +21,95 @@ function App() {
     event.preventDefault();
     setWeatherData([]);
 
-    getCurrentWeather(city)  
-      .then((res) => {
-        const currentWeather = {
-          description: res.weather[0].description.toLowerCase(),
-          temp: res.main.temp,
-          temp_max: res.main.temp_max,
-          temp_min: res.main.temp_min,
-          humidity: res.main.humidity,
-          wind: res.wind.speed,
-          sunrise: ((new Date(res.sys.sunrise * 1000)).toString()).slice(16,21),
-          sunset: ((new Date(res.sys.sunset * 1000)).toString()).slice(16,21)
-        };
+    getCurrentWeather(city).then((res) => {
+      const currentWeather = {
+        description: res.weather[0].description.toLowerCase(),
+        temp: res.main.temp,
+        temp_max: res.main.temp_max,
+        temp_min: res.main.temp_min,
+        humidity: res.main.humidity,
+        wind: res.wind.speed,
+        sunrise: new Date(res.sys.sunrise * 1000).toString().slice(16, 21),
+        sunset: new Date(res.sys.sunset * 1000).toString().slice(16, 21),
+      };
 
-        setCurrentWeather(currentWeather);
-        applyColors(currentWeather.description)
-      });
+      setCurrentWeather(currentWeather);
+      applyColors(currentWeather.description);
+    });
 
-    getCordinates(city)
-      .then((res) => {
-        const cordinatesInfo = {
-          latitude: res[0].lat,
-          longitude:res[0].lon
-        }
+    getCordinates(city).then((res) => {
+      const cordinatesInfo = {
+        latitude: res[0].lat,
+        longitude: res[0].lon,
+      };
 
-        calla(cordinatesInfo)
-        call(cordinatesInfo)
-      });
+      callHistoricalAPI(cordinatesInfo);
+      callForecastAPI(cordinatesInfo);
+    });
   };
 
-  const calla = (cordinates) => {
-    const todayTimestamp = (+Date.now() / 1000).toFixed(0);
-    const dayInSeconds = 24 * 60 * 60;
+  const callHistoricalAPI = (cordinatesInfo) => {
+    const cordinates = cordinatesInfo;
     const previousDays = 5;
+    const promises = [];
 
     for (let i = 1; i <= previousDays; i++) {
-      let referenceDay = todayTimestamp - dayInSeconds * i;
-
-      getHistoricalWeather(cordinates, referenceDay)
-        .then((res) => {
-          const sortHourTemp = res.hourly.sort(function (a, b) {
-            return a.temp < b.temp ? -1 : a.temp < b.temp ? 1 : 0;
-          });
-
-          const weatherInfo = {
-            date: (new Date(referenceDay * 1000)).toString(),
-            temp_min: sortHourTemp[0].temp,
-            temp_max: sortHourTemp[23].temp,
-            weatherDescription: res.current.weather[0].main.toLowerCase()
-          }
-
-          setWeatherData(prevState => ([...prevState, weatherInfo]))
-        });
+      promises.push(handleMultipleRequests(i, cordinates));
     }
-  }
 
-  const call = (cordinates) => {
-    getForecastWeather(cordinates)
-      .then((res) => {
-        for (let i = 0; i <= 7; i++) {
+    Promise.all(promises).then((values) => {
+      const weatherInfoArray = values.map((temp, index) => {
+        const sortHourTemp = temp.hourly.sort(function (a, b) {
+          return a.temp < b.temp ? -1 : a.temp < b.temp ? 1 : 0;
+        });
 
-          let weatherInfo = {
-            date: (new Date((res.daily[i].dt) * 1000)).toString(),
-            temp_min: res.daily[i].temp.min,
-            temp_max: res.daily[i].temp.max,
-            weatherDescription: res.current.weather[0].main.toLowerCase()
-          }
+        const referenceDay = getReferenceDay(index + 1);
 
-          setWeatherData(prevState => ([...prevState, weatherInfo]))
-        }
+        return {
+          date: new Date(referenceDay * 1000).toString(),
+          temp_min: sortHourTemp[0].temp,
+          temp_max: sortHourTemp[23].temp,
+          weatherDescription: temp.current.weather[0].main.toLowerCase(),
+        };
       });
+
+      setWeatherData(weatherInfoArray);
+    });
+  };
+
+  function handleMultipleRequests(i, cordinates) {
+    const referenceDay = getReferenceDay(i);
+    return getHistoricalWeather(cordinates, referenceDay);
   }
+
+  function getReferenceDay(i) {
+    const todayTimestamp = (+Date.now() / 1000).toFixed(0);
+    const dayInSeconds = 24 * 60 * 60;
+    return todayTimestamp - dayInSeconds * i;
+  }
+
+  const callForecastAPI = (cordinates) => {
+    getForecastWeather(cordinates).then((res) => {
+      for (let i = 0; i <= 7; i++) {
+        let weatherInfo = {
+          date: new Date(res.daily[i].dt * 1000).toString(),
+          temp_min: res.daily[i].temp.min,
+          temp_max: res.daily[i].temp.max,
+          weatherDescription: res.current.weather[0].main.toLowerCase(),
+        };
+
+        setWeatherData((prevState) => [...prevState, weatherInfo]);
+      }
+    });
+  };
 
   const applyColors = (weatherDescription) => {
     const root = document.documentElement;
-      root.style.setProperty("--bg-color", "#FCE19C");
-      root.style.setProperty("--font-color", "#312915");
-      root.style.setProperty("--icon-color", "#FFC122");
+    root.style.setProperty("--bg-color", "#FCE19C");
+    root.style.setProperty("--font-color", "#312915");
+    root.style.setProperty("--icon-color", "#FFC122");
 
-    if(weatherDescription.includes("clouds")) {
+    if (weatherDescription.includes("clouds")) {
       root.style.setProperty("--bg-color", "#D4D9E0");
       root.style.setProperty("--font-color", "#424242");
       root.style.setProperty("--icon-color", "#F0F1F2");
@@ -111,34 +126,32 @@ function App() {
       root.style.setProperty("--font-color", "424242");
       root.style.setProperty("--icon-color", "#FFFFFF");
     }
-  }
+  };
 
   const handleToggle = () => {
-    if(show) setShow(false)
-    else setShow(true)
+    if (show) setShow(false);
+    else setShow(true);
 
     toggleButton();
-  }
+  };
 
   const toggleButton = () => {
-    const getToggleContainer = document.querySelector(".details-section")
+    const getToggleContainer = document.querySelector(".details-section");
     const getButton = document.querySelector(".toggle-button");
-    const getArrow = document.querySelector(".fa-chevron-down")
+    const getArrow = document.querySelector(".fa-chevron-down");
 
     if (show) {
       getToggleContainer.classList.add("display");
       getArrow.classList.add("display");
-      getButton.innerText = "Less Info"
-    }  
-
-    else {
+      getButton.innerText = "Less Info";
+    } else {
       getToggleContainer.classList.remove("display");
       getArrow.classList.remove("display");
-      getButton.innerText = "More Info"
+      getButton.innerText = "More Info";
     }
-  }
+  };
 
-  if(weatherData.length > 0) {
+  if (weatherData.length > 0) {
     return (
       <>
         <form className="location-info" onSubmit={handleSubmit}>
@@ -155,7 +168,7 @@ function App() {
 
         <section className="container">
           <section className="resume">
-            <WeatherIcon weatherDescription={currentWeather.description}/>
+            <WeatherIcon weatherDescription={currentWeather.description} />
             <h3>{currentWeather.description}</h3>
           </section>
 
@@ -203,41 +216,45 @@ function App() {
           </section>
 
           <section className="week-section">
-            {weatherData.length > 0 && weatherData.map((item, index) => {
-              return <WeatherInfo 
-                key={index}
-                date={item.date.slice(0,3)} 
-                temp_max={item.temp_max} 
-                temp_min={item.temp_min} 
-                weatherDescription={item.weatherDescription}/>
-              })
-            }
+            {weatherData.length > 0 &&
+              weatherData.map((item, index) => {
+                return (
+                  <WeatherInfo
+                    key={index}
+                    date={item.date.slice(0, 3)}
+                    temp_max={item.temp_max}
+                    temp_min={item.temp_min}
+                    weatherDescription={item.weatherDescription}
+                  />
+                );
+              })}
           </section>
         </section>
       </>
     );
-  }
-
+  } 
   else {
     return (
       <main>
-            <section className="home-container">
-                <h1>Should I take my umbrella?</h1>
+        <section className="home-container">
+          <h1>Should I take my umbrella?</h1>
 
-                <form className="location-info" onSubmit={handleSubmit}>
-                    <label> What is your location?
-                    <input
-                        type="text"
-                        placeholder="Example: Salvador, BR"
-                        onChange={setCityInput}
-                    />
-                    </label>
-                </form>
-            </section>
+          <form className="location-info" onSubmit={handleSubmit}>
+            <label>
+              {" "}
+              What is your location?
+              <input
+                type="text"
+                placeholder="Example: Salvador, BR"
+                onChange={setCityInput}
+              />
+            </label>
+          </form>
+        </section>
 
-            <Logo />
-        </main>
-    )
+        <Logo />
+      </main>
+    );
   }
 }
 
