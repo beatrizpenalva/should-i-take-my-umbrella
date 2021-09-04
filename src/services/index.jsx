f5780158a2b5b0e9550c20f233e5e2c96f4e108f
Beatriz Penalva <biapenalva@gmail.com>
2021-09-04 10:05:35 -0300

    Update index.jsx

--- src/services/index.jsx
+++ src/services/index.jsx
@@ -12,17 +12,16 @@ export function getCordinates(city) {
     .then((response) => response.json())
 };
 
-export function getHistoricalWeather(cordinates, referenceDay) {
+export function callHistoricalAPI(cordinates, referenceDay) {
   return fetch(
     `https://api.openweathermap.org/data/2.5/onecall/timemachine?lat=${cordinates.latitude}&lon=${cordinates.longitude}&dt=${referenceDay}&units=metric&appid=7c8b054ddd8f88293b1e0e10e75ba18d`
   )
     .then((response) => response.json())
 }
 
-export function getForecastWeather(cordinates) {
+export function callForecastAPI(cordinates) {
   return fetch(
     `https://api.openweathermap.org/data/2.5/onecall?lat=${cordinates.latitude}&lon=${cordinates.longitude}&units=metric&appid=7c8b054ddd8f88293b1e0e10e75ba18d`
   )
     .then((response) => response.json())
 }
-  
