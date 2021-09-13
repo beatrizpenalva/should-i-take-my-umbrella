2cdcc3783bb4e2dd0c1f019c1c716db3dc31c4e7
Beatriz Penalva <biapenalva@gmail.com>
2021-09-13 06:53:04 -0300

    refactory: change icon color according to weather

--- src/components/WeatherIcon/index.jsx
+++ src/components/WeatherIcon/index.jsx
@@ -1,12 +1,15 @@
 import React from "react";
 import "./style.css";
 
-const WeatherIcon = ({weatherDescription, className}) => {
-    const getClass = className+"-icon"
-    if(weatherDescription.includes("clouds")) return (<i className={"fas fa-cloud weather-icon" + getClass}></i>)
-    if(weatherDescription.includes("rain")) return (<i className={"fas fa-cloud-showers-heavy weather-icon" + getClass}></i>)
-    if(weatherDescription.includes("rain")) return (<i className={"fas fa-snowflake weather-icon" + getClass}></i>)
-    else return (<i className={"fas fa-circle weather-icon" + className}></i>)
-}
+const WeatherIcon = ({ weatherDescription }) => {
+  function getIconName() {
+    if (weatherDescription.includes("clouds")) return "cloud";
+    if (weatherDescription.includes("rain")) return "cloud-showers-heavy";
+    if (weatherDescription.includes("snow")) return "snowflake";
+    else return "circle";
+  }
+
+  return <i className={`fas fa-${getIconName()} ${getIconName()}-icon`}></i>;
+};
 
 export default WeatherIcon;
